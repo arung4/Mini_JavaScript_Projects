13f327687b086c38e9d7e39a6c9db84a1ad8958f
Arun Singh Parihar <104052031+arung4@users.noreply.github.com>
2024-05-11 17:32:59 +0530

    Add files via upload

---
+++ Tabs/script.js
@@ -0,0 +1,23 @@
+// selectors 
+
+const buttonEl=document.querySelectorAll("button");
+
+const tabs= document.querySelector(".tab");
+
+const articles= document.querySelectorAll(".content");
+
+tabs.addEventListener("click",(event)=>{
+    const id= event.target.dataset.id; 
+
+    if(id){
+        buttonEl.forEach((btn)=>{
+        btn.classList.remove("live"); 
+        }); 
+        event.target.classList.add("live");
+    }
+    articles.forEach((article)=>{
+        article.classList.remove("live");
+    });
+    const element= document.getElementById(id);
+    element.classList.add("live");
+});
